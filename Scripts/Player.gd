cc172fd81bfdfb3a10a97506d26c75c36a0d803d
Jan-E-Ramos <jan.ramos8@upr.edu>
2024-06-03 15:36:29 -0400

    Movement acceleration

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-@export var speed = 16*3
+@export var speed: float = 64
+@export var accel: float = 64*(1/10)
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-	
-	#look_at(get_global_mouse_position())		#rotates player to look at mouses
+
+
 
 func _physics_process(delta):
-	get_input()
+	var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	velocity.x = move_toward(velocity.x, speed*input_direction.x, accel)
+	velocity.y = move_toward(velocity.y, speed*input_direction.y, accel)
+
 	move_and_slide()
